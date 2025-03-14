a12d87d9762974fbd526e8a96380a10a153e8aff
sm <onulee@naver.com>
2025-03-14 09:36:11 +0900

    thymeleaf+jpa관련

---
+++ t_thymeleafJpa/src/main/java/com/java/TThymeleafJpaApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TThymeleafJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TThymeleafJpaApplication.class, args);
+	}
+
+}
